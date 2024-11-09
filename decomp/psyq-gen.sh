dcbce038620468916bd65f4fa3906dff5e890e37
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-11-09 14:27:44 +0000

    Make psyq400.elf build deterministic

--- decomp/psyq-gen.sh
+++ decomp/psyq-gen.sh
@@ -2,4 +2,4 @@
 mipsel-linux-gnu-gcc -O0 -g -o psyq$1.elf -T psyq.ld \
     -fno-toplevel-reorder -nostartfiles -nostdlib \
     --whole-file -rdynamic -Wl,--whole-archive \
-    -Wl,-Map=psyq$1.map $(find obj/$1/lib* -name '*.o')
+    -Wl,-Map=psyq$1.map $(find obj/$1/lib* -name '*.o' | sort)
